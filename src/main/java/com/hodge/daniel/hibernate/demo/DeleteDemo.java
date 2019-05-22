7bbb75b657ad1efe9a09eb3e75d4872ea06e5080
Daniel Hodge <danielkhodge01@gmail.com>
2019-05-22 17:39:03 -0600

    first commit

--- src/main/java/com/hodge/daniel/hibernate/demo/DeleteDemo.java
+++ src/main/java/com/hodge/daniel/hibernate/demo/DeleteDemo.java
@@ -5,28 +5,39 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.hodge.daniel.hibernate.demo.entity.Instructor;
+import com.hodge.daniel.hibernate.demo.entity.InstructorDetail;
 import com.hodge.daniel.hibernate.demo.entity.Student;
 
 
-public class CreateStudentDemo {
+public class DeleteDemo {
 
 	public static void main(String[] args) {
 		
 		SessionFactory factory = new Configuration()
 				.configure("hibernate.cfg.xml")
-				.addAnnotatedClass(Student.class)
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
 				.buildSessionFactory();
 		
 		Session session = factory.getCurrentSession();
 		
-		try {
-			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Paul", "Wall", "paul@hodge.com");
-			
+		try {	
+
 			session.beginTransaction();
+				
+			int theId = 1;
+			Instructor tempInstructor = 
+					session.get(Instructor.class,  theId);
 			
-			System.out.println("Saving the Student...");
-			session.save(tempStudent);
+			System.out.println("Found instructor: " + tempInstructor);
+			
+			if (tempInstructor != null) {
+				
+				System.out.println("Deleting: " + tempInstructor);
+				
+				session.delete(tempInstructor);
+			}
 			
 			session.getTransaction().commit();
 			
--- src/main/java/com/hodge/daniel/jdbc/TestJdbc.java
+++ src/main/java/com/hodge/daniel/jdbc/TestJdbc.java
@@ -7,7 +7,7 @@ public class TestJdbc {
 
 	public static void main(String[] args) {
 		
-		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
 		String user = "hbstudent";
 		String pass = "hbstudent";
 		try {
@@ -15,7 +15,7 @@ public class TestJdbc {
 			
 			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
 			
-			System.out.println("Connection successful!!!");
+			System.out.println(myConn);
 		} catch(Exception exc) {
 			exc.printStackTrace();
 		}
